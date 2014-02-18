4b9d884feced4077dc23ee2a114c74cb1e79f022
Etherfly <etherfly@gmail.com>
2014-02-18 22:33:35 +0400

    Additional commit for the previous change.

---
+++ resources/other/battlegauge/transparent.bat
@@ -0,0 +1,3 @@
+# convert iconItem.png -transparent white iconItem.png
+convert iconAuraImpact1.png -transparent white iconAuraImpact1.png
+convert iconAuraImpact2.png -transparent white iconAuraImpact2.png
